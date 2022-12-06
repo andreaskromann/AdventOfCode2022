18be09a659176b278ede39da6648192f9246278d
Andreas Kromann <kromann.andreas@gmail.com>
2022-12-06 16:19:00 +0100

    Day 6 part 1

---
+++ Problems/Day06/Day6.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode2022.Problems.Day06;
+
+class Day6 : ICodingProblem
+{
+    public void Run()
+    {
+        var signal = File.ReadAllText("Problems\\Day06\\input.txt").Trim();
+        var counts = new Dictionary<char, int>();
+        for (var index = 0; index < signal.Length; index++)
+        {
+            if (index > 3)
+            {
+                var toBeRemoved = signal[index - 4];
+                counts[toBeRemoved] -= 1;
+            }
+
+            var toBeAdded = signal[index];
+            if (!counts.ContainsKey(toBeAdded))
+                counts[toBeAdded] = 0;
+            counts[toBeAdded] += 1;
+
+            if (index >= 3 && counts.Values.All(x => x < 2))
+            {
+                Console.WriteLine($"Part 1: {index + 1}");
+                break;
+            }
+        }
+    }
+}
