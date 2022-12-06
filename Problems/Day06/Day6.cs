01a4520e265abff3bffdb7c31a5804c6062f1155
Andreas Kromann <kromann.andreas@gmail.com>
2022-12-06 16:27:58 +0100

    Day 6 part 2

--- Problems/Day06/Day6.cs
+++ Problems/Day06/Day6.cs
@@ -5,12 +5,18 @@ class Day6 : ICodingProblem
     public void Run()
     {
         var signal = File.ReadAllText("Problems\\Day06\\input.txt").Trim();
+        SearchForUniqueString(signal, 4, 1);
+        SearchForUniqueString(signal, 14, 2);
+    }
+
+    private static void SearchForUniqueString(string signal, int patternLength, int partNumber)
+    {
         var counts = new Dictionary<char, int>();
         for (var index = 0; index < signal.Length; index++)
         {
-            if (index > 3)
+            if (index > patternLength - 1)
             {
-                var toBeRemoved = signal[index - 4];
+                var toBeRemoved = signal[index - patternLength];
                 counts[toBeRemoved] -= 1;
             }
 
@@ -19,10 +25,10 @@ class Day6 : ICodingProblem
                 counts[toBeAdded] = 0;
             counts[toBeAdded] += 1;
 
-            if (index >= 3 && counts.Values.All(x => x < 2))
+            if (index >= patternLength - 1 && counts.Values.All(x => x < 2))
             {
-                Console.WriteLine($"Part 1: {index + 1}");
-                break;
+                Console.WriteLine($"Part {partNumber}: {index + 1}");
+                return;
             }
         }
     }
