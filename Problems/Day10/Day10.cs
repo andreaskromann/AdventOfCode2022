c71790c09be838eaead1fffaa2af606c7cf19fec
Andreas Kromann <kromann.andreas@gmail.com>
2022-12-10 08:59:30 +0100

    Day 10 part 2

--- Problems/Day10/Day10.cs
+++ Problems/Day10/Day10.cs
@@ -8,12 +8,42 @@ class Day10 : ICodingProblem
         var register = 1;
         var cycle = 0;
         var sum = 0;
+        var crt = Enumerable.Range(1, 240).Select(_ => '.').ToArray();
+
+        void print()
+        {
+            Console.WriteLine($"Cycle: {cycle}");
+            Console.WriteLine("Sprite position:");
+            for (var i = 0; i < 40; i++)
+            {
+                if (i >= register - 1 && i <= register + 1)
+                    Console.Write('#');
+                else
+                    Console.Write('.');
+            }
+            Console.Write('\n');
+
+            Console.WriteLine("CRT status:");
+            for (var i = 0; i < crt.Length; i++)
+            {
+                Console.Write(crt[i]);
+                if ((i + 1) % 40 == 0)
+                    Console.Write('\n');
+            }
+        }
 
         void incCycle()
         {
             cycle++;
             if (cycle == 20 || (cycle >= 60 && (cycle - 60) % 40 == 0))
                 sum += cycle * register;
+            
+            var currentPosition = cycle - 1;
+            var row = currentPosition / 40;
+            if (currentPosition >= row * 40 + register - 1 && currentPosition <= row * 40 + register + 1)
+                crt[currentPosition] = '#';
+            
+            //print();
         }
         
         foreach (var line in lines)
@@ -31,5 +61,7 @@ class Day10 : ICodingProblem
         }
         
         Console.WriteLine($"Part 1: {sum}");
+        Console.WriteLine("Part 2:");
+        print();
     }
 }
