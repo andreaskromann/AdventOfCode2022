ac577a9939002ec58307ff936461f4ce7d6393e0
Andreas Kromann <kromann.andreas@gmail.com>
2022-12-12 21:36:32 +0100

    Day 12 part 2

--- Problems/Day12/Day12.cs
+++ Problems/Day12/Day12.cs
@@ -12,23 +12,20 @@ class Day12 : ICodingProblem
         // https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
         
         var unvisitedNodes = new List<Node>();
-        Node? destinationNode = null; 
         for (var y = 0; y < height; y++)
         {
             for (var x = 0; x < width; x++)
             {
                 var node = new Node
                 {
-                    Symbol = rows[y][x], 
-                    Distance = rows[y][x] == 'S' ? 0 : int.MaxValue, 
+                    Symbol = rows[y][x] == 'S' ? 'a' : (rows[y][x] == 'E' ? 'z' : rows[y][x]), 
+                    Distance = rows[y][x] == 'E' ? 0 : int.MaxValue, 
                     Visited = false, 
                     X = x, 
                     Y = y
                 };
                 map[x, y] = node;
                 unvisitedNodes.Add(node);
-                if (node.Symbol == 'E')
-                    destinationNode = node;
             }
         }
 
@@ -37,32 +34,31 @@ class Day12 : ICodingProblem
             var current = unvisitedNodes.MinBy(x => x.Distance);
             var x = current!.X;
             var y = current.Y;
-            if (x > 0 && HasEdge(map[x - 1, y].Symbol, current.Symbol))
+            if (x > 0 && map[x - 1, y].Symbol - current.Symbol >= -1)
                 map[x - 1, y].Distance = Math.Min(map[x - 1, y].Distance, current.Distance + 1);
-            if (x < width - 1 && HasEdge(map[x + 1, y].Symbol, current.Symbol))
-                map[x + 1, y].Distance =  Math.Min(map[x + 1, y].Distance, current.Distance + 1);
-            if (y > 0 && HasEdge(map[x, y - 1].Symbol, current.Symbol))
+            if (x < width - 1 && map[x + 1, y].Symbol - current.Symbol >= -1)
+                map[x + 1, y].Distance = Math.Min(map[x + 1, y].Distance, current.Distance + 1);
+            if (y > 0 && map[x, y - 1].Symbol - current.Symbol >= -1)
                 map[x, y - 1].Distance = Math.Min(map[x, y - 1].Distance, current.Distance + 1);
-            if (y < height - 1 && HasEdge(map[x, y + 1].Symbol, current.Symbol))
-                map[x, y + 1].Distance =  Math.Min(map[x, y + 1].Distance, current.Distance + 1);
+            if (y < height - 1 && map[x, y + 1].Symbol - current.Symbol >= -1)
+                map[x, y + 1].Distance = Math.Min(map[x, y + 1].Distance, current.Distance + 1);
             current.Visited = true;
             unvisitedNodes.Remove(current);
-        } while (!destinationNode!.Visited && unvisitedNodes.Min(x => x.Distance) != int.MaxValue);
-        
-        Console.WriteLine($"Part 1: {destinationNode.Distance}");
-    }
+        } while (unvisitedNodes.Any() &&
+                 !unvisitedNodes.Where(x => x.Symbol == 'a').All(x => x.Visited) &&
+                 unvisitedNodes.Min(x => x.Distance) != int.MaxValue);
 
-    private bool HasEdge(char to, char from)
-    {
-        if (from == 'S')
-            from = 'a';
-        if (from == 'E')
-            from = 'z';
-        if (to == 'S')
-            to = 'a';
-        if (to == 'E')
-            to = 'z';
-        return to - from <= 1;
+        Node? minNode = null;
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                if (map[x, y].Symbol == 'a' && (minNode is null || minNode.Distance > map[x, y].Distance))
+                    minNode = map[x, y];
+            }
+        }
+
+        Console.WriteLine($"Part 2: {minNode!.Distance}");
     }
 
     class Node
